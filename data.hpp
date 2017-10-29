093108a8ef3cd2eb9d3f88de499977a612e713b7
Honglin Yu <yuhonglin1986@gmail.com>
2017-10-30 00:13:10 +1100

    make it work for m=4,n=2

--- data.hpp
+++ data.hpp
@@ -266,13 +266,15 @@ public:
 };
 
 struct model {
+  const std::vector<double> beta;
   std::vector<double> beta;
   profile get_next_state(const profile& p, const profile& a) {
+    const double d1 = 0;
     return profile();
   }
-  std::vector<double> get_profit(const profile& p, const profile& a) {
-    return std::vector<double>();
-  }
+    std::vector<double> get_profit(const profile& p, const profile& a) {
+      return std::vector<double>();
+    }
 };
 
 struct alg {
