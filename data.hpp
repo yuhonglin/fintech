2b8c269a15a7e827da0cbc91a6f625c98f8b97f3
Honglin Yu <yuhonglin1986@gmail.com>
2017-10-29 23:49:27 +1100

    solve special case m=4, n=2

--- data.hpp
+++ data.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <numeric>
 
 // basic data structure
 struct array {
@@ -287,8 +288,21 @@ struct alg {
 
   std::vector<double> R;
     
-  alg(const func_prof& s, const func_prof& a) : func_state(s), func_action(a) {};
+  alg(const func_prof& s, const func_prof& a) : func_state(s), func_action(a) {
+    init_R();
+  };
 
+  void init_R() {
+    if (m!=4 or n!=2) {
+      std::cerr<< "only support m=4 and n=2" << endl;
+    }
+    R = { -1,0,
+	  1,0,
+	  0,1,
+	  0,-1 };
+    min_b_idx = {0, 3}; // MAYBE WRONG
+  };
+  
   // set the first part of A matrix of constraint
   void set_A_1(std::vector<double>& A) {
     // The first part is just the R matrix
@@ -302,13 +316,10 @@ struct alg {
     // The second part of A is even easier
     int idx = R.size();
     for (int i=0; i<n; i++) { // for each agent
-      for (int j=0; j<func_action.card()-1; j++) {
-	A[idx] = -config.beta[i];
-	idx += n;
-      }
-      idx++;
+      A[idx] = -config.beta[i];
+      idx += n+1;
     }
-  }
+  };
 
   // set the first part of b
   void set_b_1(std::vector<double>& b, const profile& state_prof,
@@ -327,22 +338,46 @@ struct alg {
     std::vector<double> crnt_profit = config.get_profit(state_prof, action_prof);
 	
     profile iter;
-    int idx = 0;
     for (int i=0; i<n; i++) { // for each agent
       iter = action_prof;
       func_action.reset(iter, i);
+      double minvalue = std::numeric_limits<double>::max();
       for ( ; iter!=func_action.end(); func_action.inc_only(iter,i) ) {
-	if (iter == action_prof)
-	  continue;
 	std::vector<double> iter_profit = config.get_profit(state_prof, iter);
 	profile iter_nxtstat = config.get_next_state(state_prof, iter);
 	double min_b = W[iter_nxtstat.index()*m + min_b_idx[i]];
-	b[idx] = (1-config.beta[i])*(crnt_profit[i]-iter_profit[i]) - config.beta[i]*min_b;
-	idx++;
+	double tmp = (1-config.beta[i])*(crnt_profit[i]-iter_profit[i]) - config.beta[i]*min_b;
+	if (minvalue > tmp) {
+	  tmp = minvalue;
+	}
+      }
+      b[i] = minvalue;
+    }
+  };
+
+  void linprog(const int&n, const int&m, const double* r,
+	       const double* A, const double* b, const double& f,
+	       const int &status) {
+    if (m!=4 || n!=2) {
+      std::cerr<< "only support m=4 and n=2" << endl;
+    }
+    // check feasibility
+    if (b[1] < b[4]/A[8] || b[2] < b[5]/A[10]) {
+      status = -1;
+      f = std::numeric_limits<double>::min();
+      return;
+    }
+    
+    f = 0;
+    for (int i=0; i<n; i++) {
+      if (r[i] > 0) {
+	f += r[i]*b[i+1]; // b's upper bound
+      } else {
+	f += r[i]*std::max(-b[3*i], b[m+i]/A[R.size()+i*n+i]); // b's lower bound
       }
     }
-  }
-
+  };
+  
   void solve() {
     std::vector<double> W(func_state.card()*m, 5);
     std::vector<double> W_new(W);
@@ -358,12 +393,14 @@ struct alg {
 	  // wks, a per-thread vector to avoid false sharing
 	  std::vector<double> wks(func_action.card());
 	  // the constraints
-	  std::vector<double> A((n*(func_action.card()-1) + m)*n, 0.);
-	  std::vector<double> b(n*(func_action.card()-1) + m);
+	  std::vector<double> A((n + m)*n, 0.);
+	  std::vector<double> b(n + m);
 	  // set the first part of A
 	  set_A_1(A);
 	  // set the second part of A
 	  set_A_2(A);
+
+	  int idx = 0;
 	  for ( ; action_prof != func_action.end(); func_action.inc(action_prof)) {
 	    // generate first part of b
 	    set_b_1(b, state_prof, action_prof, W);
@@ -371,11 +408,11 @@ struct alg {
 	    set_b_2(b, state_prof, action_prof, W);
 			
 	    // do the linear programming
-	    // TODO
-	    
+	    double f;
+	    linprog(n, m, r, A, b, f);
 	    
 	    // store optimal value to wks
-	    // TODO
+	    wks[i] = f;
 	  }
 	  // find the maximum of wks
 	  W_new[state_prof.index()*m+i] = *std::max(wks.begin(), wks.end());
