1c13c7605b5038dac3bcc72777f02f0b8ff16112
Honglin Yu <honglin.yu@anu.edu.au>
2018-01-15 14:42:29 +1100

    attache the missing files.

---
+++ util.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <sstream>
+
+#include <glob.h>
+
+#include "util.hpp"
+  
+std::stack<std::clock_t> util::_bnc_prof_tstart;
+
+void util::tic() {
+  _bnc_prof_tstart.push(clock());
+}
+
+void util::toc() {
+  std::clock_t tend = clock();
+  std::cout << "toc: " << (static_cast<double>(tend-_bnc_prof_tstart.top())/CLOCKS_PER_SEC)
+	    << " second(s)." << std::endl;
+  _bnc_prof_tstart.pop();
+}
+    
+
+std::vector<std::string> util::glob(const std::string& pat) {
+  using namespace std;
+  glob_t glob_result;
+  ::glob(pat.c_str(),GLOB_TILDE,NULL,&glob_result);
+  vector<string> ret;
+  for(unsigned int i=0;i<glob_result.gl_pathc;++i){
+    ret.push_back(string(glob_result.gl_pathv[i]));
+  }
+  globfree(&glob_result);
+  return ret;
+}
+
+template<typename Out>
+void util::split(const std::string &s, char delim, Out result) {
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        *(result++) = item;
+    }
+}
+
+std::vector<std::string> util::split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, std::back_inserter(elems));
+    return elems;
+}
---
+++ util.hpp
@@ -0,0 +1,27 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+#include <stack>
+#include <ctime>
+#include <iostream>
+#include <string>
+
+
+class util {
+public:
+  // profiling
+  static std::stack<std::clock_t> _bnc_prof_tstart;
+  static void tic();
+  static void toc();
+  
+  // file system
+  static std::vector<std::string> glob(const std::string& pat);
+
+  // string
+  template<typename Out>
+  static void split(const std::string &s, char delim, Out result);
+  static std::vector<std::string> split(const std::string &s, char delim);
+  
+};  // class util
+
+#endif /* _UTIL_H_ */
